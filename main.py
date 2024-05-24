8f778997876ee1b77a70b0eabaedbf8d8c3892c0
shizukani-cp <to.maimai0606@gmail.com>
2024-05-24 19:10:34 +0900

    メインループを作成

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ if len(sys.argv) == 2:
         sys.exit("Directory not found.")
 elif len(sys.argv) > 2:
     sys.exit("Too many arguments.")
+
+while True:
+    print(os.getcwd())
+    print("0\t..")
+    for i in range(len(os.listdir())):
+        print(str(i + 1) + "\t" + os.listdir()[i])
+    c = input("> ")
+    try:
+        try:
+            os.chdir((["../"] + os.listdir())[int(c)])
+        except NotADirectoryError:
+            sys.exit("It's canno't openable.")
+    except ValueError:
+        if c == "e":
+            break
+        else:
+            sys.exit("Command not found.")
+sys.exit(0)
