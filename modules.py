0e9538bf811bb4525d33e0dc530e42ad0ad52701
shizukani-cp <to.maimai0606@gmail.com>
2024-05-26 04:49:39 +0900

    clipboardのサブコマンドを追加

--- modules.py
+++ modules.py
@@ -5,8 +5,15 @@ clipboard_contents = EMPTY_CLIPBOARD_CONTENTS
 
 def clipboard(command: str):
     args = command.split(" ")
-    if clipboard_contents == EMPTY_CLIPBOARD_CONTENTS:
-        sys.exit("\x1b[31mThere are no items on the clipboard.\x1b[0m")
+    if args[1] == "v":
+        if clipboard_contents == EMPTY_CLIPBOARD_CONTENTS:
+            sys.exit("\x1b[31mThere are no items on the clipboard.\x1b[0m")
+    elif args[1] == "c":
+        pass
+    elif args[1] == "x":
+        pass
+    else:
+        sys.exit("\x1b[31mSubcommand not found.\x1b[0m")
     
 
 cTof = {
