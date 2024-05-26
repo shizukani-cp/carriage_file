d25dc0829185b4fca53138efb3938cfeaf40aca5
shizukani-cp <to.maimai0606@gmail.com>
2024-05-26 12:48:35 +0900

    ペースト処理を追加

--- modules.py
+++ modules.py
@@ -7,6 +7,8 @@ def clipboard(args: list[str]):
     if args[1] == "v":
         if clipboard_contents == EMPTY_CLIPBOARD_CONTENTS:
             sys.exit("\x1b[31mThere are no items on the clipboard.\x1b[0m")
+        with open(clipboard_contents["fname"], "xb") as f:
+            f.write(clipboard_contents["conetent"])
     elif args[1] == "c":
         pass
     elif args[1] == "x":
