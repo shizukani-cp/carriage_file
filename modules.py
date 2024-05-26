595471272dac55737d8ad04dedc724b4bbb8cf37
shizukani-cp <to.maimai0606@gmail.com>
2024-05-26 13:04:05 +0900

    ファイル名を番号で指定するよう修正
    ファイル名を番号で指定するよう修正
    
    インデックスを書く場所を修正

--- modules.py
+++ modules.py
@@ -5,7 +5,7 @@ clipboard_contents = EMPTY_CLIPBOARD_CONTENTS
 
 def clipboard(args: list[str]):
     try:
-        if not os.path.isfile(args[2]):
+        if not os.path.isfile(os.listdir()[args[2]]):
             sys.exit("\x1b[31mIs not file.\x1b[0m")
     except IndexError:
         sys.exit("\x1b[31mMissing arguments.\x1b[0m")
@@ -15,14 +15,14 @@ def clipboard(args: list[str]):
         with open(clipboard_contents["fname"], "xb") as f:
             f.write(clipboard_contents["conetent"])
     elif args[1] == "c":
-        with open(args[2], "rb"):
-            clipboard_contents["fname"] = args[2]
+        with open(os.listdir()[args[2]], "rb"):
+            clipboard_contents["fname"] = os.listdir()[args[2]]
             clipboard_contents["conetent"] = f.read()
     elif args[1] == "x":
-        with open(args[2], "rb"):
-            clipboard_contents["fname"] = args[2]
+        with open(os.listdir()[args[2]], "rb"):
+            clipboard_contents["fname"] = os.listdir()[args[2]]
             clipboard_contents["conetent"] = f.read()
-        os.remove(args[2])
+        os.remove(os.listdir()[args[2]])
     else:
         sys.exit("\x1b[31mSubcommand not found.\x1b[0m")
 
