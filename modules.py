e20024f423cc98cd63b110974595a863475103a5
shizukani-cp <to.maimai0606@gmail.com>
2024-05-26 14:21:47 +0900

    ミスを修正
    not忘れを修正
    
    ミスを修正

--- modules.py
+++ modules.py
@@ -4,25 +4,30 @@ EMPTY_CLIPBOARD_CONTENTS = {"fname":None,"content":None}
 clipboard_contents = EMPTY_CLIPBOARD_CONTENTS
 
 def clipboard(args: list[str]):
-    try:
-        if not os.path.isfile(os.listdir()[int(args[2]) - 1]):
-            sys.exit("\x1b[31mIs not file.\x1b[0m")
-    except IndexError:
-        sys.exit("\x1b[31mMissing arguments.\x1b[0m")
     if args[1] == "v":
-        if clipboard_contents == EMPTY_CLIPBOARD_CONTENTS:
+        if not clipboard_contents == EMPTY_CLIPBOARD_CONTENTS:
             sys.exit("\x1b[31mThere are no items on the clipboard.\x1b[0m")
         with open(clipboard_contents["fname"], "xb") as f:
-            f.write(clipboard_contents["conetent"])
+            f.write(clipboard_contents["content"])
     elif args[1] == "c":
-        with open(os.listdir()[args[2]], "rb"):
-            clipboard_contents["fname"] = os.listdir()[args[2]]
-            clipboard_contents["conetent"] = f.read()
+        try:
+         if not os.path.isfile(os.listdir()[int(args[2]) - 1]):
+            sys.exit("\x1b[31mIs not file.\x1b[0m")
+        except IndexError:
+            sys.exit("\x1b[31mMissing arguments.\x1b[0m")
+        with open(os.listdir()[int(args[2]) - 1], "rb") as f:
+            clipboard_contents["fname"] = os.listdir()[int(args[2]) - 1]
+            clipboard_contents["content"] = f.read()
     elif args[1] == "x":
-        with open(os.listdir()[args[2]], "rb"):
-            clipboard_contents["fname"] = os.listdir()[args[2]]
-            clipboard_contents["conetent"] = f.read()
-        os.remove(os.listdir()[args[2]])
+        try:
+         if not os.path.isfile(os.listdir()[int(args[2]) - 1]):
+            sys.exit("\x1b[31mIs not file.\x1b[0m")
+        except IndexError:
+            sys.exit("\x1b[31mMissing arguments.\x1b[0m")
+        with open(os.listdir()[int(args[2]) - 1], "rb") as f:
+            clipboard_contents["fname"] = os.listdir()[int(args[2]) - 1]
+            clipboard_contents["content"] = f.read()
+        os.remove(os.listdir()[int(args[2]) - 1])
     else:
         sys.exit("\x1b[31mSubcommand not found.\x1b[0m")
 
