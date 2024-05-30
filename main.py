39ebb8106bd47c47c7b3044b8e1e09943624464a
shizukani-cp <to.maimai0606@gmail.com>
2024-05-30 15:18:35 +0900

    カラーリングをcoloring.coloringでするよう修正

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import sys, os, subprocess
-import modules, coloring
+import modules
+from coloring import coloring
 
 if len(sys.argv) == 2:
     try:
         os.chdir(sys.argv[1])
     except FileNotFoundError:
-        sys.exit("\x1b[31mDirectory not found.\x1b[0m")
+        sys.exit(coloring("Directory not found.", "error"))
 elif len(sys.argv) > 2:
-    sys.exit("\x1b[31mToo many arguments.\x1b[0m")
+    sys.exit(coloring("Too many arguments.", "error"))
 
 while True:
-    print("\x1b[34m" + os.getcwd() + "\x1b[0m")
-    print("0\t\x1b[32m..\x1b[0m")
+    print(coloring(os.getcwd(), "chdir"))
+    print(coloring("..", "dir"))
     for i in range(len(os.listdir())):
         print(str(i + 1) + "\t", end="")
         if os.path.isfile(os.listdir()[i]):
-            print("\x1b[33m" + os.listdir()[i] + "\x1b[0m")
+            print(coloring(os.listdir[i], "file"))
         else:
-            print("\x1b[32m" + os.listdir()[i] + "\x1b[0m")
+            print(coloring(os.listdir()[i], "dir"))
     c = input("> ").split(" ")
     try:
         os.chdir((["../"] + os.listdir())[int(c[0])])
--- modules.py
+++ modules.py
@@ -10,33 +10,33 @@ def num_to_name(n: int):
 def clipboard(args: list[str]):
     if args[1] == "v":
         if not clipboard_contents == EMPTY_CLIPBOARD_CONTENTS:
-            sys.exit("\x1b[31mThere are no items on the clipboard.\x1b[0m")
+            sys.exit(coloring("There are no items on the clipboard.", "error"))
         try:
             with open(clipboard_contents["fname"], "xb") as f:
                 f.write(clipboard_contents["content"])
         except FileExistsError:
-            sys.exit("\x1b[31mFile already exists.\x1b[0m")
+            sys.exit(coloring("File already exists.", "error"))
     elif args[1] == "c":
         try:
          if not os.path.isfile(num_to_name(args[2])):
-            sys.exit("\x1b[31mIs not file.\x1b[0m")
+            sys.exit(coloring("Is not file.", "error"))
         except IndexError:
-            sys.exit("\x1b[31mMissing arguments.\x1b[0m")
+            sys.exit(coloring("Missing arguments.", "error"))
         with open(num_to_name(args[2]), "rb") as f:
             clipboard_contents["fname"] = num_to_name(args[2])
             clipboard_contents["content"] = f.read()
     elif args[1] == "x":
         try:
          if not os.path.isfile(num_to_name(args[2])):
-            sys.exit("\x1b[31mIs not file.\x1b[0m")
+            sys.exit(coloring("Is not file.", "error"))
         except IndexError:
-            sys.exit("\x1b[31mMissing arguments.\x1b[0m")
+            sys.exit(coloring("Missing arguments.", "error"))
         with open(num_to_name(args[2]), "rb") as f:
             clipboard_contents["fname"] = num_to_name(args[2])
             clipboard_contents["content"] = f.read()
         os.remove(num_to_name(args[2]))
     else:
-        sys.exit("\x1b[31mSubcommand not found.\x1b[0m")
+        sys.exit(coloring("Subcommand not found.", "error"))
 
 def delete(args: list[str]):
     if os.path.isfile(num_to_name(args[1])):
@@ -52,7 +52,7 @@ def newfile(args: list[str]):
         with open(args[1], "x"):
             pass
     except FileExistsError:
-        sys.exit("\x1b[31mFile already exists.\x1b[0m")
+        sys.exit(coloring("File already exists.", "error"))
 
 def copy_chdir_command(args: list[str]):
     pyperclip.copy(f"cd {os.getcwd()}")
@@ -76,6 +76,6 @@ def main(args: list[str]):
     try:
         f = cTof[args[0]]
     except KeyError:
-        sys.exit("\x1b[31mCommand not found.\x1b[0m")
+        sys.exit(coloring("Command not found.", "error"))
     else:
         f(args)
